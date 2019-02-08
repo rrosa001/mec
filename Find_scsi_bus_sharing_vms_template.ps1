08b828be44e80a6722df1bc3c5665b16bc0dac76
ricardorosa <Ricardo.Rosa@cdillc.com>
2019-02-08 12:53:40 -0500

    Create Find_scsi_bus_sharing_vms_template.ps1
    SCSI Bus Sharing  Report

---
+++ Find_scsi_bus_sharing_vms_template.ps1
@@ -0,0 +1,39 @@
+$array = @()
+
+$vms = get-cluster “ClusterName” | get-vm
+
+#Loop for BusSharingMode
+
+foreach ($vm in $vms)
+
+{
+
+ 
+
+$disks = $vm | Get-ScsiController | Where-Object {$_.BusSharingMode -eq ‘Physical’ -or $_.BusSharingMode -eq ‘Virtual’}
+
+ 
+
+foreach ($disk in $disks){
+
+$REPORT = New-Object -TypeName PSObject
+
+$REPORT | Add-Member -type NoteProperty -name Name -Value $vm.Name
+
+$REPORT | Add-Member -type NoteProperty -name VMHost -Value $vm.Host
+
+$REPORT | Add-Member -type NoteProperty -name Mode -Value $disk.BusSharingMode
+
+$REPORT | Add-Member -type NoteProperty -name Type -Value “BusSharing”
+
+$array += $REPORT
+
+}
+
+ 
+
+ 
+
+}
+
+$array | out-gridview
